d66e8a8b17e6e760c333a55d9528dfe229f6d343
VNATdev-ops <vuanhtuan18092004@gmail.com>
2025-11-23 16:44:56 +0700

    clear codebase

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-// Export all models
-module.exports = {
-  User: require('./User'),
-  Subject: require('./Subject'),
-  Question: require('./Question'),
-  Exam: require('./Exam'),
-  ExamResult: require('./ExamResult')
-};
